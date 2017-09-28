beea6be4f50b43716b5af632791c80c3679fd6f5
George Paul <george.allen.paul.3@gmail.com>
2017-09-28 18:48:37 +0200

    Add test for rounding down to nearest decimal in percent convert func

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const fs = require('fs')
 
 const findHighestValue = array => Math.max(...array)
 
+const convertDecimalToPerc = num => Math.floor((num * 100) * 100) / 100
+
+const findPositiveReturn = array => {
+  const posiAryLen = array.filter(num => num > 0 || num === 0).length
+  return posiAryLen / array.length
+}
+
 const readText = fileName => {
   fs.readFile(fileName, (err, file) => {
     const ary = file.toString().split('\n')
     console.log(ary[0])
     console.log('Year: ', ary[1])
     const returnValues = ary[2].split(', ')
+    const percentPositiveMonths = `${convertDecimalToPerc(findPositiveReturn(returnValues))}%`
+    console.log(`% Positive Months: ${percentPositiveMonths}`)
     console.log(`Best Month: ${findHighestValue(returnValues)}%`)
   })
 }
@@ -23,5 +32,7 @@ runChallenge()
 module.exports = {
   runChallenge,
   readText,
-  findHighestValue
+  findHighestValue,
+  findPositiveReturn,
+  convertDecimalToPerc
 }
--- test.js
+++ test.js
@@ -1,18 +1,42 @@
 const assert = require('assert')
 const index = require('./index')
 const findHighestValue = index.findHighestValue
+const findPositiveReturn = index.findPositiveReturn
+const convertDecimalToPerc = index.convertDecimalToPerc
 
-describe('findHighestValue', function() {
-  describe('#findHighestValue()', function() {
-    it("should return the highest value from an array", function() {
+describe('convertDecimalToPerc', function () {
+  describe('#convertDecimalToPerc()', function () {
+    it('should round down to the nearest 2 decimal', function () {
+      const num = 0.66667
+      assert.equal(convertDecimalToPerc(num), 66.66)
+    })
+    it('should convert a decimal to a percentage number', function () {
+      const num = 0.32
+      assert.equal(convertDecimalToPerc(num), 32)
+    })
+  })
+})
+
+describe('findPositiveReturn', function () {
+  describe('#findPositiveReturn()', function () {
+    it('should return the percentage of returns that are positive', function () {
+      const myArray = [1, 2, 4, -2, -3, -1]
+      assert.equal(findPositiveReturn(myArray), 0.5)
+    })
+  })
+})
+
+describe('findHighestValue', function () {
+  describe('#findHighestValue()', function () {
+    it("should return the highest value from an array", function () {
       const myArray = [1, 3, 2, 0.4, 8]
       assert.equal(findHighestValue(myArray), 8)
     })
-    it("should return the highest value from an array with negative values", function() {
+    it("should return the highest value from an array with negative values", function () {
       const myArray = [-5.07, -0.41, 6.60, 0.27, 1.53, 0.09, 3.56, -0.12, -0.12, -1.94, 3.42, 1.82]
       assert.equal(findHighestValue(myArray), 6.60)
     })
-    it("should return the highest value from an array of strings", function() {
+    it("should return the highest value from an array of strings", function () {
       const myArray = ['1', '3', '2', '0.4', '8', '-2']
       assert.equal(findHighestValue(myArray), 8)
     })
