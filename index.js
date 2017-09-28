ecf154acd55ebdd9071506b7a19958f4754425c4
George Paul <george.allen.paul.3@gmail.com>
2017-09-28 18:13:22 +0200

    Remove console.log testing, add test for highest value

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const runChallenge = () => {
-  console.log('hello')
-  // return 'hello'
+const fs = require('fs')
+
+const readText = fileName => {
+  fs.readFile(fileName, (err, file) => {
+    const ary = file.toString().split('\n')
+    console.log(ary[0])
+    console.log('Year: ', ary[1])
+  })
 }
+
+const runChallenge = () => {
+  readText('./input_one.txt')
+  readText('./input_two.txt')
+}
+
+const findHighestValue = array => array
+
 runChallenge()
 
 module.exports = {
-  runChallenge
+  runChallenge,
+  readText,
+  findHighestValue
 }
--- test.js
+++ test.js
@@ -2,17 +2,14 @@ const assert = require('assert')
 const sinon = require('sinon')
 const index = require('./index')
 const runChallenge = index.runChallenge
+const readText = index.readText
+const findHighestValue = index.findHighestValue
 
-describe('runChallenge', function() {
-  describe('#runChallenge()', function() {
-    it('should return string that says hello', function() {
-      assert.equal(runChallenge(), 'hello')
-    })
-
-    it('should console.log hello', function() {
-      let spy = sinon.spy(console, 'log')
-      runChallenge()
-      assert(spy.calledWith('hello'))
+describe('findHighestValue', function() {
+  describe('#findHighestValue()', function() {
+    it("should return the highest value from an array", function() {
+      const myArray = [1, 3, 2, 0.4, 8]
+      assert.equal(findHighestValue(myArray), 8)
     })
   })
 })
